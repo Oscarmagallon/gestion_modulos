7c899b621e7326633f49dc1d8cd078cf6dadfd76
oscar <oscarmagallon2002@hotmail.com>
2021-01-20 12:22:32 +0100

    finalizacion v2.0

--- src/gestion_modulos/Principal.java
+++ src/gestion_modulos/Principal.java
@@ -5,9 +5,24 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Modulos m = new Modulos("Programacion","PGR", 2);
+		int aux=0;
+		Modulos p = new Modulos("Programacion","PGR", 2);
+		Modulos b = new Modulos("Base de datos","BDD", 1);
+		Modulos l = new Modulos("Lenguaje Marcas","LM", 3);
+		
+		Modulos vModulos[]=new Modulos[7];
+		vModulos[0]= p;
+		vModulos[1]= b;
+		vModulos[2]= l;
 		
 		
+	
+for (Modulos m : vModulos) {
+	if(m!=null) {
+			aux = m.getHoras() + aux;
+	}
+		}
+System.out.println("El numero de horas totales son: "+ aux);
 	}
 	
 
