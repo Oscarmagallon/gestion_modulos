c9c56d1f3726ce9ff676c8ec759f6b39936832b7
oscar <oscarmagallon2002@hotmail.com>
2021-01-20 11:42:16 +0100

    finalizacion v.1

--- src/gestion_modulos/Modulos.java
+++ src/gestion_modulos/Modulos.java
@@ -1,5 +1,18 @@
 package gestion_modulos;
 
 public class Modulos {
+	private String nombre;
+	private String codigo;
+	private int horas;
+	
+	public Modulos(String nombre, String codigo, int horas) {
+		this.nombre = nombre;
+		this.codigo = codigo;
+		this.horas = horas;
+	}
+	
+	
 
+
+	
 }
---
+++ src/gestion_modulos/Principal.java
@@ -0,0 +1,11 @@
+package gestion_modulos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Modulos m = new Modulos("Programacion","PGR1", "2");
+	}
+
+}
